83d7964c19bd26d7ed2b40979c8c3b0eaa245f6d
Ermyas62 <ermyas62@gmail.com>
2024-01-25 23:59:24 -0800

    Zombie

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,33 @@
+/**
+ * infinite_while - function to sleep
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - entry to the program
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int children_processes = 0;
+	pid_t pid;
+
+	while (children_processes < 5)
+	{
+		pid = fork();
+		if (!pid)
+			break;
+		printf("Zombie process created, PID: ZOMBIE_PID", (int)pid);
+	}
+	if (pid != 0)
+		infinite_white();
+	return (0);
+}
