f39e7e1c4b778b6210dfeeb2587574d746de1f8a
Ermyas62 <ermyas62@gmail.com>
2024-03-22 23:49:21 -0700

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,9 +2,9 @@
 
 file { 'school':
 ensure	=> 'present',
-path	=> '/tmp/school',
-mode	=> '0744',
-owner	=> 'www-data',
-group	=> 'www-data',
-content	=> 'I love Puppet'
+path 	=> '/tmp/school',
+mode 	=> '0744',
+owner 	=> 'www-data',
+group 	=> 'www-data',
+content => 'I love Puppet'
 }
